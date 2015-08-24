ca0264281c4f055198bf222ed2b0f5092b89b03e
tigrangs <programtg@yahoo.com>
2015-08-24 20:36:26 +0400

    move files to core folder

--- thesis/main.cpp
+++ thesis/main.cpp
@@ -1,14 +1,27 @@
 #include "sorted_matrix.hpp"
 #include "matrix_analyzer.hpp"
+#include "analize_manager.hpp"
+
 
 int main()
 {
-    sorted_matrix m(10);
+    typedef analize_manager AM;
+    AM* am = AM::get_instance();
+    am->set_internal_matrix_size(5);
+    am->set_matrix_count(100000);
+    am->set_matrix_size(20, 20);
+    am->set_value_range(0, 100000000);
+    am->run();
+    const AM::data& d = am->get_analized_data();
+    for (AM::data::const_iterator i = d.begin(); i != d.end(); ++i) {
+//        (*i).print();
+    }
+/*    sorted_matrix m(10);
     m.fill_with_sorting(2,0,100000000);
     m.print();
     typedef matrix_analyzer MA;
     MA* ma = MA::get_instance();
     analyzed_data d = ma->analyze(&m, 2);
-    d.print();
+    d.print();*/
     return 0;
 }
